9ca553edd812a407d746dcca1cbcfa15a64e31cc
2018011038 <zhang-tl18@mails.tsinghua.edu.cn>
2021-03-31 13:26:40 +0800

    run successfully

--- PA1/odd_even_sort.cpp
+++ PA1/odd_even_sort.cpp
@@ -1,3 +1,5 @@
+//考虑n/nprocs不为整数
+//先判断是否逆序，再传data
 #include <algorithm>
 #include <cassert>
 #include <cstdio>
@@ -10,8 +12,12 @@ void Worker::sort() {
     /** Your code ... */
     // you can use variables in class Worker: 
     //n, nprocs, rank, block_len, data, last_rank
-    float *recv_buffer = new float[block_len];
-    void merge(float* my_data, float* recv_data, int mode, int block_len);
+
+    size_t block_size = ceiling(n, nprocs);    //可能last_rank的block_len < block_size
+    size_t last_size = n - block_size * (nprocs - 1);
+    float recv_float;
+    float *recv_buffer = new float[block_size];
+    void merge(float* my_data, float* recv_data, int my_len, int recv_len, int mode);
     int getPartner(int i_case, int rank);
       
     //进程内排序
@@ -21,9 +27,26 @@ void Worker::sort() {
     for (int i = 0; i < nprocs; i++){
         int partner = getPartner(i, rank);
         if(partner >= 0 && partner < nprocs){
-            MPI_Send(data, block_len, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
-            MPI_Recv(recv_buffer, block_len, MPI_FLOAT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            merge(data, recv_buffer, block_len, rank - partner);
+            if(partner < rank){
+                MPI_Send(&data[0], 1, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
+                MPI_Recv(&recv_float, 1, MPI_FLOAT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if(recv_float > data[0]){
+                    MPI_Send(data, block_len, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
+                    MPI_Recv(recv_buffer, block_size, MPI_FLOAT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    int recv_len = (partner == nprocs - 1)? last_size: block_size;
+                    merge(data, recv_buffer, block_len, recv_len, rank - partner);
+                }
+            }
+            else{
+                MPI_Send(&data[block_len - 1], 1, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
+                MPI_Recv(&recv_float, 1, MPI_FLOAT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                if(recv_float < data[block_len - 1]){
+                    MPI_Send(data, block_len, MPI_FLOAT, partner, 0, MPI_COMM_WORLD);
+                    MPI_Recv(recv_buffer, block_size, MPI_FLOAT, partner, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                    int recv_len = (partner == nprocs - 1)? last_size: block_size;
+                    merge(data, recv_buffer, block_len, recv_len, rank - partner);
+                }
+            }
         }
     } 
     delete []recv_buffer;
@@ -44,14 +67,14 @@ int getPartner(int i_case, int rank){
     }
 }
 
-void merge(float* my_data, float* recv_data, int block_len, int mode){
+void merge(float* my_data, float* recv_data, int my_len, int recv_len, int mode){
     int mi, ti, ri;
-    float *temp = new float[block_len];
+    float *temp = new float[my_len];
 
     if (mode < 0){
         mi = ri = ti = 0;
-        while (ti < block_len){
-            if (mi < block_len && ri < block_len){
+        while (ti < my_len){
+            if (mi < my_len && ri < recv_len){
                 if (my_data[mi] >= recv_data[ri]){
                     temp[ti] = recv_data[ri];
                     ri++;
@@ -63,12 +86,12 @@ void merge(float* my_data, float* recv_data, int block_len, int mode){
                     mi++;
                 }
             }
-            else if (mi < block_len){
+            else if (mi < my_len){
                 temp[ti] = my_data[mi];
                 ti++;
                 mi++;
             }
-            else if (ri < block_len){
+            else if (ri < recv_len){
                 temp[ti] = recv_data[ri];
                 ti++;
                 ri++;
@@ -76,7 +99,8 @@ void merge(float* my_data, float* recv_data, int block_len, int mode){
         }
     }
     else{
-        mi = ri = ti = block_len - 1;
+        mi = ti = my_len - 1;
+        ri = recv_len - 1;
         while (ti >= 0){
             if (mi >= 0 && ri >= 0){
                 if (my_data[mi] < recv_data[ri]){
@@ -103,7 +127,7 @@ void merge(float* my_data, float* recv_data, int block_len, int mode){
         }
     }
 
-    for (mi = 0; mi < block_len; mi++)
+    for (mi = 0; mi < my_len; mi++)
         my_data[mi] = temp[mi];
     
     delete []temp;
